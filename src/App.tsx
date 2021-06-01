38cf1a2b78a8af4867d54f390fdfabf20ad22c49
cindy-sf <c.saintfleurant@gmail.com>
2021-06-01 23:24:46 +0200

    fix: remove useless Text attribute

--- src/App.tsx
+++ src/App.tsx
@@ -14,10 +14,6 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
-    justifyContent: 'center',
-
-    Text: {
-
-    }
+    justifyContent: 'center'
   },
 });
