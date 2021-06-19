835f619db653e6f387f6a3ccdf1f1c79b9154a1c
cindy-sf <c.saintfleurant@gmail.com>
2021-06-19 21:39:59 +0200

    refactor: reviews changes

--- src/components/Error/index.tsx
+++ src/components/Error/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { View } from 'react-native'
+import styled from 'styled-components/native'
 
 import ErrorImage from '@assets/images/error/error_image.png'
 
@@ -8,17 +8,30 @@ import Image from '@components/Image'
 import Layout from '@components/Layout'
 import Text from '@components/Text'
 
+const TitleWrapper = styled.View`
+  flex: 2px;
+`
+
+const Separator = styled.View`
+  flex: 0.4px;
+`
+
+const BottomArea = styled.View`
+  align-items: center;
+  flex: 0.5px;
+`
+
 const Error = ({ navigation }: { navigation: any }) => (
   <Layout>
-    <View style={{ flex: 2 }}>
+    <TitleWrapper>
       <Text font="POPPINS_SEMI_BOLD" size="HEADLINE_1" maxWidth={320}>
         Navré, une erreur est survenue...
       </Text>
-      <View style={{ flex: 0.4 }} />
+      <Separator />
       <Image src={ErrorImage} height={285} width={270} resizeMode="cover" />
-      <View style={{ flex: 0.4 }} />
-    </View>
-    <View style={{ alignItems: 'center', flex: 0.5 }}>
+      <Separator />
+    </TitleWrapper>
+    <BottomArea>
       <Button
         onPress={() =>
           navigation.reset({
@@ -29,7 +42,7 @@ const Error = ({ navigation }: { navigation: any }) => (
       >
         Revenir à l’accueil
       </Button>
-    </View>
+    </BottomArea>
   </Layout>
 )
 
