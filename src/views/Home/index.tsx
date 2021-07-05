800ab145de8ee960ef2453800e5f111ee8ff8aa3
cindy-sf <c.saintfleurant@gmail.com>
2021-07-05 12:59:55 +0200

    fix: actuality typo mistakes

--- src/views/Home/index.tsx
+++ src/views/Home/index.tsx
@@ -96,7 +96,7 @@ const Home = ({ navigation }: { navigation: NavigationContainerRef }) => {
               <SpotlightCard news={news[0]} />
               <View style={{ marginTop: spaces.LARGE }} />
               <Text font="POPPINS_SEMI_BOLD" size="HEADLINE_2" textAlign="left">
-                Actualité
+                Actualités
               </Text>
               <ScrollView
                 showsVerticalScrollIndicator={false}
