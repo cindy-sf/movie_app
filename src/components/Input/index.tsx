8ca376dfcee0a77061307a4ce3abcc918f66faec
cindy-sf <c.saintfleurant@gmail.com>
2021-07-07 10:05:55 +0200

    feat: adding secure text entry option on input component

--- src/components/Input/index.tsx
+++ src/components/Input/index.tsx
@@ -1,7 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { NativeSyntheticEvent, TextInputChangeEventData } from 'react-native'
 import { useSelector } from 'react-redux'
 import styled from 'styled-components/native'
+
+import ShowPasswordIconWhite from '@assets/icons/show_password_icon_white.png'
+import ShowPasswordIconBlack from '@assets/icons/show_password_icon_black.png'
+import HidePasswordIconWhite from '@assets/icons/hide_password_icon_white.png'
+import HidePasswordIconBlack from '@assets/icons/hide_password_icon_black.png'
+
+import Image from '@components/Image'
 
 import {
   colors,
@@ -16,6 +23,7 @@ interface Props {
   placeHolder: string
   value: string
   onChange: (e: NativeSyntheticEvent<TextInputChangeEventData>) => void
+  secureTextEntry?: boolean
 }
 
 const TextInput = styled.TextInput`
@@ -27,7 +35,6 @@ const TextInput = styled.TextInput`
   border-style: solid;
   border-radius: ${radius.XX_LARGE}px;
   color: ${({ theme }: { theme: ThemeAttributes }) => theme.PRIMARY_TEXT_COLOR};
-  margin-top: ${spaces.LARGE}px;
   padding-top: ${spaces.SMALL}px;
   padding-bottom: ${spaces.SMALL}px;
   padding-left: ${spaces.LARGE}px;
@@ -35,19 +42,60 @@ const TextInput = styled.TextInput`
   font-size: ${fontSize.OVERLINE}px;
 `
 
-const Input = ({ placeHolder, value, onChange }: Props) => {
+const InputWrapper = styled.View`
+  position: relative;
+  margin-top: ${spaces.LARGE}px;
+`
+
+const IconWrapper = styled.TouchableOpacity`
+  position: absolute;
+  top: 13px;
+  right: 26px;
+  z-index: 10;
+  elevation: 10;
+`
+
+const Input = ({ placeHolder, value, onChange, secureTextEntry }: Props) => {
   const appTheme = useSelector(
     ({ theme }: { theme: ThemeAttributes }) => theme.mode
   )
+  const [isPasswordTextHidden, setIsPasswordTextHidden] =
+    useState(secureTextEntry)
+
+  const showPasswordIcon =
+    appTheme === 'light' ? ShowPasswordIconBlack : ShowPasswordIconWhite
+  const hidePasswordIcon =
+    appTheme === 'light' ? HidePasswordIconBlack : HidePasswordIconWhite
 
   return (
-    <TextInput
-      value={value}
-      onChange={onChange}
-      placeholder={placeHolder}
-      placeholderTextColor={appTheme === 'light' ? colors.BLACK : colors.WHITE}
-    />
+    <InputWrapper>
+      <TextInput
+        value={value}
+        onChange={onChange}
+        placeholder={placeHolder}
+        placeholderTextColor={
+          appTheme === 'light' ? colors.BLACK : colors.WHITE
+        }
+        secureTextEntry={isPasswordTextHidden}
+      />
+      {secureTextEntry && (
+        <IconWrapper
+          onPress={() => setIsPasswordTextHidden(!isPasswordTextHidden)}
+        >
+          <Image
+            width={24}
+            height={24}
+            src={isPasswordTextHidden ? hidePasswordIcon : showPasswordIcon}
+            resizeMode="cover"
+          />
+        </IconWrapper>
+      )}
+    </InputWrapper>
   )
+}
+
+Input.defaultProps = {
+  secureTextEntry: false,
 }
 
 export default Input
