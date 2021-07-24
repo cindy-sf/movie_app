6a48d6607044d5a6d20c1d841bc7988f50e4eb9c
cindy-sf <c.saintfleurant@gmail.com>
2021-07-24 13:19:47 +0200

    feat: adding BottomSheets component

--- src/styles/theme.ts
+++ src/styles/theme.ts
@@ -8,6 +8,7 @@ export type ThemeModeType = 'dark' | 'light'
 export interface ThemeAttributes {
   mode: ThemeModeType
   BACKGROUND: string
+  BOTTOM_SHEET_BACKGROUD: string
   AVATAR_BORDER: string
   INPUT_BACKGROUND: string
   INPUT_BORDER_COLOR: string
@@ -91,6 +92,7 @@ export const themes: Themes = {
 export const darkTheme: ThemeAttributes = {
   mode: themes.DARK,
   BACKGROUND: colors.BLACK_MEDIUM,
+  BOTTOM_SHEET_BACKGROUD: colors.BLACK,
   AVATAR_BORDER: colors.PURPLE,
   INPUT_BACKGROUND: colors.GREY_DARK,
   INPUT_BORDER_COLOR: colors.WHITE,
@@ -106,6 +108,7 @@ export const darkTheme: ThemeAttributes = {
 export const lightTheme: ThemeAttributes = {
   mode: themes.LIGHT,
   BACKGROUND: colors.WHITE,
+  BOTTOM_SHEET_BACKGROUD: colors.WHITE,
   AVATAR_BORDER: colors.PURPLE,
   INPUT_BACKGROUND: colors.WHITE,
   INPUT_BORDER_COLOR: colors.BLACK,
