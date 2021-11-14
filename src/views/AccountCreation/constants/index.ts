13f51bbd7d4c6121af2824633a3c4a73fdb33c9e
cindy-sf <c.saintfleurant@gmail.com>
2021-11-14 18:46:14 +0100

    feat: adding more user pictures and optimized it

--- src/views/AccountCreation/constants/index.ts
+++ src/views/AccountCreation/constants/index.ts
@@ -1,13 +1,23 @@
 import { ImageSourcePropType } from 'react-native'
 
+import ToyFaces_Colored_BG_1 from '@assets/images/user_pictures/ToyFaces_Colored_BG_1.png'
+import ToyFaces_Colored_BG_2 from '@assets/images/user_pictures/ToyFaces_Colored_BG_2.png'
+import ToyFaces_Colored_BG_3 from '@assets/images/user_pictures/ToyFaces_Colored_BG_3.png'
+import ToyFaces_Colored_BG_4 from '@assets/images/user_pictures/ToyFaces_Colored_BG_4.png'
+import ToyFaces_Colored_BG_5 from '@assets/images/user_pictures/ToyFaces_Colored_BG_5.png'
+import ToyFaces_Colored_BG_6 from '@assets/images/user_pictures/ToyFaces_Colored_BG_6.png'
+import ToyFaces_Colored_BG_7 from '@assets/images/user_pictures/ToyFaces_Colored_BG_7.png'
+import ToyFaces_Colored_BG_9 from '@assets/images/user_pictures/ToyFaces_Colored_BG_9.png'
+import ToyFaces_Colored_BG_10 from '@assets/images/user_pictures/ToyFaces_Colored_BG_10.png'
+import ToyFaces_Colored_BG_11 from '@assets/images/user_pictures/ToyFaces_Colored_BG_11.png'
+import ToyFaces_Colored_BG_12 from '@assets/images/user_pictures/ToyFaces_Colored_BG_12.png'
+import ToyFaces_Colored_BG_13 from '@assets/images/user_pictures/ToyFaces_Colored_BG_13.png'
 import ToyFaces_Colored_BG_47 from '@assets/images/user_pictures/ToyFaces_Colored_BG_47.png'
 import ToyFaces_Colored_BG_8 from '@assets/images/user_pictures/ToyFaces_Colored_BG_8.png'
 import ToyFaces_Colored_BG_29 from '@assets/images/user_pictures/ToyFaces_Colored_BG_29.png'
 import ToyFaces_Colored_BG_32 from '@assets/images/user_pictures/ToyFaces_Colored_BG_32.png'
-import ToyFaces_Colored_BG_37 from '@assets/images/user_pictures/ToyFaces_Colored_BG_37.png'
 import ToyFaces_Colored_BG_49 from '@assets/images/user_pictures/ToyFaces_Colored_BG_49.png'
 import ToyFaces_Colored_BG_56 from '@assets/images/user_pictures/ToyFaces_Colored_BG_56.png'
-import ToyFaces_Colored_BG_59 from '@assets/images/user_pictures/ToyFaces_Colored_BG_59.png'
 
 export interface UserPictures {
   pictureUrl: ImageSourcePropType
@@ -16,11 +26,21 @@ export interface UserPictures {
 
 export const userPictures: UserPictures[] = [
   { pictureUrl: ToyFaces_Colored_BG_47, imageName: 'ToyFaces_Colored_BG_47' },
+  { pictureUrl: ToyFaces_Colored_BG_4, imageName: 'ToyFaces_Colored_BG_4' },
+  { pictureUrl: ToyFaces_Colored_BG_5, imageName: 'ToyFaces_Colored_BG_5' },
+  { pictureUrl: ToyFaces_Colored_BG_6, imageName: 'ToyFaces_Colored_BG_6' },
+  { pictureUrl: ToyFaces_Colored_BG_7, imageName: 'ToyFaces_Colored_BG_7' },
+  { pictureUrl: ToyFaces_Colored_BG_9, imageName: 'ToyFaces_Colored_BG_9' },
+  { pictureUrl: ToyFaces_Colored_BG_10, imageName: 'ToyFaces_Colored_BG_10' },
   { pictureUrl: ToyFaces_Colored_BG_8, imageName: 'ToyFaces_Colored_BG_8' },
   { pictureUrl: ToyFaces_Colored_BG_29, imageName: 'ToyFaces_Colored_BG_29' },
   { pictureUrl: ToyFaces_Colored_BG_32, imageName: 'ToyFaces_Colored_BG_32' },
-  { pictureUrl: ToyFaces_Colored_BG_37, imageName: 'ToyFaces_Colored_BG_37' },
   { pictureUrl: ToyFaces_Colored_BG_49, imageName: 'ToyFaces_Colored_BG_49' },
   { pictureUrl: ToyFaces_Colored_BG_56, imageName: 'ToyFaces_Colored_BG_56' },
-  { pictureUrl: ToyFaces_Colored_BG_59, imageName: 'ToyFaces_Colored_BG_59' },
+  { pictureUrl: ToyFaces_Colored_BG_1, imageName: 'ToyFaces_Colored_BG_1' },
+  { pictureUrl: ToyFaces_Colored_BG_2, imageName: 'ToyFaces_Colored_BG_2' },
+  { pictureUrl: ToyFaces_Colored_BG_3, imageName: 'ToyFaces_Colored_BG_3' },
+  { pictureUrl: ToyFaces_Colored_BG_11, imageName: 'ToyFaces_Colored_BG_11' },
+  { pictureUrl: ToyFaces_Colored_BG_12, imageName: 'ToyFaces_Colored_BG_12' },
+  { pictureUrl: ToyFaces_Colored_BG_13, imageName: 'ToyFaces_Colored_BG_13' },
 ]
