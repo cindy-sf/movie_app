548ada7d2cae12478865d367bbf961d206d6eee3
fr-jsf <jonathan.saintfleurant@gmail.com>
2022-01-16 16:16:11 +0100

    refactor: use navigation hook

--- src/views/ComingSoon/index.tsx
+++ src/views/ComingSoon/index.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import styled from 'styled-components/native'
-import type { NavigationContainerRef } from '@react-navigation/native'
+import { useNavigation } from '@react-navigation/native'
 
 import Button from '@components/Button'
 import Image from '@components/Image'
@@ -27,33 +27,32 @@ const ImageWrapper = styled.View`
   margin-bottom: ${spaces.LARGE}px;
 `
 
-interface Props {
-  navigation: NavigationContainerRef
+const ComingSoon = () => {
+  const navigation = useNavigation()
+  return (
+    <Layout
+      headerOptions={{
+        closeIcon: { onClose: () => navigation.goBack() },
+      }}
+    >
+      <Separator>
+        <Text font="POPPINS_SEMI_BOLD" size="HEADLINE_1" maxWidth={290}>
+          De belles choses arrivent !
+        </Text>
+      </Separator>
+      <Container>
+        <ImageWrapper>
+          <Image src={ToTheMoon} width={354} height={248} />
+        </ImageWrapper>
+        <Text font="POPPINS_MEDIUM" size="BODY_2" maxWidth={315}>
+          Prochainement, consulter les actus{'\n'}séries
+        </Text>
+      </Container>
+      <Separator>
+        <Button onPress={navigation.goBack}>Ok !</Button>
+      </Separator>
+    </Layout>
+  )
 }
-
-const ComingSoon = ({ navigation }: Props) => (
-  <Layout
-    headerOptions={{
-      closeIcon: { onClose: () => navigation.goBack() },
-    }}
-  >
-    <Separator>
-      <Text font="POPPINS_SEMI_BOLD" size="HEADLINE_1" maxWidth={290}>
-        De belles choses arrivent !
-      </Text>
-    </Separator>
-    <Container>
-      <ImageWrapper>
-        <Image src={ToTheMoon} width={354} height={248} />
-      </ImageWrapper>
-      <Text font="POPPINS_MEDIUM" size="BODY_2" maxWidth={315}>
-        Prochainement, consulter les actus{'\n'}séries
-      </Text>
-    </Container>
-    <Separator>
-      <Button onPress={navigation.goBack}>Ok !</Button>
-    </Separator>
-  </Layout>
-)
 
 export default ComingSoon
